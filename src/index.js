53dd1c571458d99a30be1376cee59714c047bfca
Leth4L <carribean_08@mail.ru>
2021-03-04 14:42:35 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return Math.min.apply( Math, array );
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    return Math.min.apply( Math, array );
+  }
 }
 
 exports.max = function max (array) {
-  return Math.max.apply( Math, array );
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    return Math.max.apply( Math, array );
+  }
+  
 }
 
 exports.avg = function avg (array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
   return array.reduce((a, b) => (a + b)) / array.length;
+  }
 }
